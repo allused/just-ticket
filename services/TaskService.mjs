956af09252f4aeb2aee8e0dda7af73b2feb2a353
allused <divixed12@gmail.com>
2023-02-27 13:50:01 +0100

    Add AddTask, GetTask methods to Task service layer

---
+++ services/TaskService.mjs
@@ -0,0 +1,30 @@
+import {getSpiraPool} from './DbConnection.mjs'
+
+const pool = getSpiraPool();
+
+const AddTask = async (task) =>
+{
+    try 
+    {
+        return await pool.query(`INSERT INTO task(displayname, teamid, userid, description, createdate, tasktypeid,taskpriorityid) VALUES ($1,$2,$3,$4,$5,$6,$7)`)
+    } 
+    catch (error) 
+    {
+        console.log(`The following error occured while adding a task to DB: ${error}`);
+    }
+}
+
+const GetTask = async (getParam) =>
+{
+    const condition = "userId" in getParam ? "userid" : "teamid";
+    try 
+    {
+        return await pool.query(`SELECT * FROM task WHERE ${condition}= $1`,["userId" in getParam ? getParam["userId"] : getParam["teamId"]])
+    } 
+    catch (error) 
+    {
+        console.log(`The following error occured while getting task: ${error}`)
+    }
+}
+
+export{AddTask, GetTask}
