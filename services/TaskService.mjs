d16b79749e4f0a7f210e5c062388723cc146ef07
allused <divixed12@gmail.com>
2023-02-27 14:13:09 +0100

    Add new paramateres to AddTask, Add UpdateTask method, which updates an existing task

--- services/TaskService.mjs
+++ services/TaskService.mjs
@@ -6,7 +6,7 @@ const AddTask = async (task) =>
 {
     try 
     {
-        return await pool.query(`INSERT INTO task(displayname, teamid, userid, description, createdate, tasktypeid,taskpriorityid) VALUES ($1,$2,$3,$4,$5,$6,$7)`, task)
+        return await pool.query(`INSERT INTO task(displayname, teamid, createuserid, assigneuserid, description, createdate, tasktypeid, taskpriorityid, taskstateid,uploadfilename) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`, task);
     } 
     catch (error) 
     {
@@ -19,12 +19,23 @@ const GetTask = async (getParam) =>
     const condition = "userId" in getParam ? "userid" : "teamid";
     try 
     {
-        return await pool.query(`SELECT * FROM task WHERE ${condition}= $1`,["userId" in getParam ? getParam["userId"] : getParam["teamId"]])
+        return await pool.query(`SELECT * FROM task WHERE ${condition}= $1`,["userId" in getParam ? getParam["userId"] : getParam["teamId"]]);
     } 
     catch (error) 
     {
-        console.log(`The following error occured while getting task: ${error}`)
+        console.log(`The following error occured while getting task: ${error}`);
     }
 }
 
-export{AddTask, GetTask}
+const UpdateTask = async (task) =>
+{
+    try 
+    {
+        return await pool.query(`UPDATE task SET createuserid=$2,assignuserid=$3,displayname=$4,description=$5,tasktypeid=$6,taskpriorityid=$7,taskstateid=$8,uploadfilename=$9,modifieddate=$10 WHERE id=$1 RETURNING displayname, teamid, createuserid, assigneuserid, description, tasktypeid, taskpriorityid, taskstateid,uploadfilename`, task);  
+    } 
+    catch (error) {
+        console.log(`The following error occured while updating task: ${error}`);
+    }
+}
+
+export{AddTask, GetTask, UpdateTask}
