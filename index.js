466c8b0b9199f968623d5c317356a3b6ed851bcb
allused <divixed12@gmail.com>
2023-03-03 15:37:10 +0100

    Add task routers to app route

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import Joi from 'joi';
+import { taskRouter } from './routes/TaskRoutes.mjs';
 import { userRouter } from './routes/UserRoutes.mjs';
 
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use('/api', userRouter);
+app.use('/api', taskRouter);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
