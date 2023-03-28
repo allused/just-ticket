20c5d25cd0cb5ef6aca630d11be8f3927f597882
allused <divixed12@gmail.com>
2023-03-28 12:26:27 +0200

    Add comment router to app routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Joi from 'joi';
+import { commentRouter } from './routes/CommentRoutes.mjs';
 import { taskRouter } from './routes/TaskRoutes.mjs';
 import { userRouter } from './routes/UserRoutes.mjs';
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', userRouter);
 app.use('/api', taskRouter);
+app.use('/api', commentRouter);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
