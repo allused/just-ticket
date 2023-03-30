4e98c7a5e9fc143a4292a0ad49714f44ef29aaff
allused <divixed12@gmail.com>
2023-03-30 15:19:28 +0200

    Replace patch arrow function to the comment controller function call to update comment

--- routes/CommentRoutes.mjs
+++ routes/CommentRoutes.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addNewComment, AddNewComment, getCommentByTaskID } from '../controllers/CommentController.mjs';
+import { addNewComment, AddNewComment, getCommentByTaskID, patchCommentByID } from '../controllers/CommentController.mjs';
 
 const commentRouter = express.Router();
 
@@ -7,32 +7,6 @@ commentRouter.get('/api/tasks/:taskId/comments', getCommentByTaskID);
 
 commentRouter.post('/api/tasks/:taskId/comments', addNewComment);
 
-commentRouter.patch('/api/comments/:commentId', (req, resp) => {
-
-    const {commentId} = req.params;
-    if (commentId != null && Joi.validate(req.body, commentSchemaPatch))
-    {
-        const comment = null;
-        //Return the proper http response with the updated comment if the update was succesful
-        if (comment != null){
-
-            if (dontHavePermission)
-            {
-                resp.status(403).send(responseMessages.forbidden)
-            }
-            else
-            {
-                resp.status(201).send(`The following comment is succesfully updated: \n${comment}`)
-            }
-            
-        }else{
-            resp.status(404).send(responseMessages.internalServerError)
-        }
-    } 
-    else
-    {
-        resp.status(400).send(responseMessages.badRequestObj)
-    }
-    });
+commentRouter.patch('/api/comments/:commentId', patchCommentByID);
 
 export{commentRouter}
