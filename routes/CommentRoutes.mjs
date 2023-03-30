5329c6b9f85e60f7844c3067e5ffef2906fb3423
allused <divixed12@gmail.com>
2023-03-30 14:47:45 +0200

    Update GET method with the right controller function, add function the POST method

--- routes/CommentRoutes.mjs
+++ routes/CommentRoutes.mjs
@@ -1,67 +1,13 @@
-import {app} from '../index.js';
-import { responseMessages } from './HttpRespMsgs.mjs';
-import {idSchema, commentSchemaPost, commentSchemaPatch, commentSchemaGet} from '../validators/ModelValidations.mjs';
+import express from 'express';
+import { addNewComment, AddNewComment, getCommentByTaskID } from '../controllers/CommentController.mjs';
 
-app.get('/api/tasks/:taskId/comments', (req, resp) => {
+const commentRouter = express.Router();
 
-    const {taskId} = req.params;
-    if (taskId != null && Joi.validate(req.body, commentSchemaGet))
-    {
+commentRouter.get('/api/tasks/:taskId/comments', getCommentByTaskID);
 
-        //Calling the comment controller below, to get the requested comment, 
-        
-        const comment = null;
-        //When the comment is returned, we send it in a response, if not found, send the proper rest response that not found
-        if (comment != null){
-            //Check if the client has access to the required action, if yes send the querry result, if no send the proper rest response 
-            if (dontHavePermission)
-            {
-                resp.status(403).send(responseMessages.forbidden)
-            }
-            else
-            {
-                resp.status(200).send(comment)
-            }
-        }else{
-            resp.status(404).send(responseMessages.notFound)
-        }
-    } 
-    else
-    {
-        resp.status(400).send(responseMessages.badRequestId)
-    }
-     });
+commentRouter.post('/api/tasks/:taskId/comments', addNewComment);
 
-app.post('/api/tasks/:taskId/comments', (req, resp) => {
-
-    const {taskId} = req.params;
-    if (taskId != null && Joi.validate(req.body, commentSchemaPost))
-    {
-        
-        //When the comment is added succesfully to the DB, its returned, otherwise we get a null value
-        const comment = null;
-        if (comment != null){
-            //Checking if the user is authorized to make the requested action, and send the proper http response 
-            if (dontHavePermission)
-            {
-                resp.status(403).send(responseMessages.forbidden)
-            }
-            else
-            {
-                resp.status(201).send(`The following comment is succesfully created: \n${comment}`)
-            }
-            
-        }else{
-            resp.status(500).send(responseMessages.internalServerError)
-        }
-    } 
-    else
-    {
-        resp.status(400).send(responseMessages.badRequestObj)
-    }
-    });
-
-app.patch('/api/comments/:commentId', (req, resp) => {
+commentRouter.patch('/api/comments/:commentId', (req, resp) => {
 
     const {commentId} = req.params;
     if (commentId != null && Joi.validate(req.body, commentSchemaPatch))
@@ -88,3 +34,5 @@ app.patch('/api/comments/:commentId', (req, resp) => {
         resp.status(400).send(responseMessages.badRequestObj)
     }
     });
+
+export{commentRouter}
